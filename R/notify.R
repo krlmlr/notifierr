15843473c5858eb4f52550028e8470ff78a43ede
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-22 12:27:45 +0200

    initial implementation

---
+++ R/notify.R
@@ -0,0 +1,11 @@
+#' Create a desktop notification
+#'
+#' This function creates a desktop notification, currently using the
+#' \code{notify-send} command.
+#'
+#' @param subject,body Message contents
+notify <- function(subject, body = NULL) {
+  if (is.null(body))
+    body <- ""
+  system2("notify-send", c(shQuote(subject), shQuote(body)))
+}
