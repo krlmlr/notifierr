4ec27c616615d897a20f8097f08fe96a4d78ec69
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-22 13:56:44 +0200

    default argument for subject

--- R/notify.R
+++ R/notify.R
@@ -5,7 +5,9 @@
 #'
 #' @param subject,body Message contents
 #' @export
-notify <- function(subject, body = NULL) {
+notify <- function(subject = NULL, body = NULL) {
+  if (is.null(subject))
+    subject <- "notifierr"
   if (is.null(body))
     body <- ""
   system2("notify-send", c(shQuote(subject), shQuote(body)))
